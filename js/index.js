fa16dd2f187d082fd085cf42ffe31079555f4392
fenggj <394010345@qq.com>
2016-12-05 01:33:26 +0800

    update message-list

--- js/index.js
+++ js/index.js
@@ -14,60 +14,76 @@ function size(originSize, type) {
 $(function () {
     size(1334, "y")
 
-    var flag=true;
+    var flag = true;
     $(".mask").hide()
-    touch.on($('nav .close'),"tap",function(){
-        $('nav .close').toggleClass("active");
+    touch.on($('#news-h nav .close'), "tap", function () {
+        $('#news-h nav .close').toggleClass("active");
 
-        if($('nav .close').attr("class")=="close active"){
-            $('nav .close').css("opacity","1")
+        if ($('#news-h nav .close').attr("class") == "close active") {
+            $('#news-h nav .close').css("opacity", "1")
             $(".nav-left").addClass("active");
             $(".mask").show().addClass("show");
             $("body").css({
-                overflow:"hidden"
+                overflow: "hidden"
             })
-            flag=false;
+            flag = false;
             clearInterval(t);
-        }else{
-            $('nav .close').css("opacity","0")
+        } else {
+            $('#news-h nav .close').css("opacity", "0")
             $("body").css({
-                overflow:"scroll"
+                overflow: "scroll"
             });
             $(".nav-left").removeClass("active");
             $(".mask").removeClass("show");
 
         }
     })
-    touch.on($('.mask'),"touchstart",function(){
-        if(flag){
+    touch.on($('.mask'), "touchstart", function () {
+        if (flag) {
             return;
         }
-        flag=true;
+        flag = true;
         $("body").css({
-            overflow:"scroll"
+            overflow: "scroll"
         });
-        $('nav .close').css({"opacity":"0",transitionDelay:"1s","-webkit-transition-delay":"1s"});
-        $('nav .close').removeClass("active");
-            $(".nav-left").removeClass("active");
-            $(".mask").removeClass("show");
+        $('#news-h nav .close').css({"opacity": "0", transitionDelay: "1s", "-webkit-transition-delay": "1s"});
+        $('#news-h nav .close').removeClass("active");
+        $(".nav-left").removeClass("active");
+        $(".mask").removeClass("show");
     })
-    $('.mask')[0].addEventListener("webkitTransitionEnd",function(){
-        if($('nav .close').attr("class")=="close active"){
+    if ($('.mask')[0]) {
+        $('.mask')[0].addEventListener("webkitTransitionEnd", function () {
+            if ($('#news-h nav .close').attr("class") == "close active") {
 
-        }else{
-            $(".mask").hide()
-        }
-    })
+            } else {
+                $(".mask").hide()
+            }
+        })
+    } else {
+    }
     $(".nav-items li").click(function () {
         $(".nav-items li").removeClass("active");
         $(this).addClass("active");
     })
-    touch.on($(".nav-items li"),"tap",function(){
+    touch.on($(".nav-items li"), "tap", function () {
 
     })
-    var t=setTimeout(function(){
-        $("#news-h .close").css("opacity","0");
+    var t = setTimeout(function () {
+        $("#news-h .close").css("opacity", "0");
         clearInterval(t);
-    },3000)
+    }, 3000)
 
+    $(".fixe-close").click(function () {
+        $(this).toggleClass("active")
+    })
+
+
+
+    //留言
+
+    $(".keyword>div").click(function () {
+        $(".keyword>div").removeClass("active");
+        $(this).addClass("active");
+        location.href="message.html"
+    })
 })
